692d62150c23c1def569c8edbcbceba731c9c4fd
Treyton <Treyton88@gmail.com>
2024-05-15 13:07:59 -0600

    Hey theo :)

---
+++ app/src/main/java/com/example/treytontheoassign/CongratsActivity.java
@@ -0,0 +1,43 @@
+package com.example.treytontheoassign;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CongratsActivity extends AppCompatActivity {
+
+    private Drawable[] drawables;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_congrats);
+
+        // Initialize drawable
+        drawables = new Drawable[1];
+        // Retrieving data
+        //int correctCombos = getIntent().getIntExtra("correctCombos", 0);
+        String message = getIntent().getStringExtra("Congratulations you have finished the game");
+
+        // Initializing views
+        drawables[0] = getDrawable(R.drawable.check_mark);
+        TextView congratsMessage = findViewById(R.id.congrats_message);
+        //TextView comboCount = findViewById(R.id.combo_count);
+        Button closeButton = findViewById(R.id.close_button);
+
+        // Setting congratulatory message and correct combo count
+        congratsMessage.setText(message);
+        //comboCount.setText(getString(R.string.correct_combos, correctCombos));
+
+        // Close button click listener
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
